ba022f2651b3b5b0f009e98fd407c74da361ce94
Rachael Lunney <rlunney@gmail.com>
2019-02-03 18:26:37 -0600

    Add initial bitbucket data gathering

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
+import json
 from collections import defaultdict
 
+import requests
 from flask import Flask, jsonify
 from github import Github
 
@@ -67,9 +69,64 @@ def create_app(test_config=None):
 
         return data
 
+    def check_response_status(status_code):
+        if status_code != 200:
+            raise Exception(f'Error connecting to Bitbucket API. Status code: {response.status_code}')
+
+    def get_paginated_count(url):
+        count = 0
+        while url:
+            response = requests.get(url, headers=headers)
+            check_response_status(response.status_code)
+
+            response_data = json.loads(response.text)
+            values = response_data['values']
+            count += len(values)
+            url = response_data.next if response_data.next else None                
+        return count
+
+    def get_bitbucket_user(username):
+        BITBUCKET_BASE_URL = 'https://api.bitbucket.org/2.0'
+        headers = {'Content-Type': 'application/json'}
+
+        repo_url = BITBUCKET_BASE_URL + f'/users/{username}/repositories'
+        response = requests.get(repo_url, headers=headers)
+        check_response_status(response.status_code)
+
+        repo_data = json.loads(response.text)
+        data = {
+            'public_repos': {
+                'total': repo_data['size'],
+                'original': 0,
+                'forked': 0
+            },
+            'followers': 0,
+            'open_issues': 0,
+            'commits': 0,
+            'account_size': 0,
+            'languages': defaultdict(int),
+        }
+
+        for repo in repo_data['values']:
+            data['account_size'] += repo['size']
+
+            if repo.parent:
+                data['public_repos']['forked'] += 1
+            else:
+                data['public_repos']['original'] += 1
+                data['commits'] += get_paginated_count(repo['links']['commits'])
+
+            if repo.has_issues:
+                data['open_issues'] += get_paginated_count(repo['links']['issues'])
+
+            if repo.language:
+                    data['languages'][repo['language'] += 1
+
+        return data
+
     @app.route('/user/<username>')
     def get_user(username):
-        data = get_github_user(username)
+        data = get_bitbucket_user(username)
         return jsonify(data)
 
     return app
